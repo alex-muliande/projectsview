b196bc5f2d36b2828453be23376321ae45b74525
alex-muliande <amuliande@gmail.com>
2019-10-22 09:35:14 +0300

    Add image  objects to the views of the index

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .email import send_welcome_email
+from .models import Image
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    images = Image.objects.all()
+    return render(request,'index.html',{"images":images})

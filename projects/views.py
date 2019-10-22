7b39442be37cb98812abe841fa771444f7e21463
alex-muliande <amuliande@gmail.com>
2019-10-22 20:47:28 +0300

    Add query for the projects and add projectslist for API

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Projects,Profile
+from .serializer import ProjectsSerializer,ProfileSerializer
+
 
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
@@ -7,8 +12,11 @@ from users.forms import ProjectForm
 from .models import *
 # Create your views here.
 def index(request):
-  
-    return render(request,'index.html')
+    projects = Projects.objects.all()
+    context ={
+        "projects":projects,
+    }
+    return render(request,'index.html',context)
 
 
 
@@ -33,3 +41,17 @@ def get_project(request, id):
     project = Projects.objects.get(pk=id)
 
     return render(request, 'project.html', {'project':project})
+
+
+class ProjectsList(APIView):
+    def get(self, request, format=None):
+        all_projects = Projects.objects.all()
+        serializers = ProjectsSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)

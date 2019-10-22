8555a466381e9f13221990bc15aebef4e31b0e87
alex-muliande <amuliande@gmail.com>
2019-10-22 11:30:02 +0300

    Add urls.py with account url and imports tro urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.conf.urls import url,include
 from . import views
 urlpatterns = [
     path('',  views.index, name='projects-index'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+
 ]

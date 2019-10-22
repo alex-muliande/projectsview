a72e163d440eb9db9eeef41be1650aab0e911240
alex-muliande <amuliande@gmail.com>
2019-10-22 11:28:22 +0300

    add files for the user app

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from projects.models import Projects
 from django.contrib.auth.forms import UserCreationForm
+
 
 
 
@@ -12,4 +14,10 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class ProjectForm(forms.ModelForm):
+
+    class Meta:
+        model= Projects
+        exclude= ['author', 'created_date', 'author_profile']
 

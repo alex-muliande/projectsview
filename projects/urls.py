6ed098953cd8066a98e0bd94604238552a18df49
alex-muliande <amuliande@gmail.com>
2019-10-26 15:29:25 +0300

    Add profile path

--- projects/urls.py
+++ projects/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^api/projects/$', views.ProjectsList.as_view()),
     url(r'^api/profile/$', views.ProfileList.as_view()),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),

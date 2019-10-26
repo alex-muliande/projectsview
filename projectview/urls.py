32601aca1ac085f7b8b66edb9f39f0e2e5ed22a5
alex-muliande <amuliande@gmail.com>
2019-10-26 15:30:00 +0300

    Add aa card to the profikle page.

--- projectview/urls.py
+++ projectview/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('profile/', user_views.profile, name='profile'),
     path('logout/', auth_views.LogoutView.as_view( template_name='logout.html' ), name='logout'),
     path('', include('projects.urls')),
 ]
